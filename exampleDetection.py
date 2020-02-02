0bab77ff80332389117b8c59d2e11e72626da4c6
James Stevenson <james.l.stevenson@gmail.com>
2020-02-02 10:32:31 +0000

    Updated comments and names.

--- exampleDetection.py
+++ exampleDetection.py
@@ -2,9 +2,9 @@ from CategoryIdentifier import Identifier
 from Website import Website
 
 my_website = Website("https://www.bbc.co.uk/news/health-51345279")
-my_detector = Identifier(my_website, "newsDetector.json")
+my_identifier = Identifier(my_website, "newsDetector.json")
 
-if my_detector.is_match():
+if my_identifier.is_match():
     print("The website {} is a news website.".format(my_website.website_url))
 else:
     print("The website {} is not a news website.".format(my_website.website_url))
